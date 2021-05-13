cd86f743a06b1d4071ba6093191309b8e1b0935c
Joshua Gerrard <joshuagerrard@protonmail.com>
2021-05-13 12:31:35 +0000

    Make bot echo messages back.

--- bot.py
+++ bot.py
@@ -33,10 +33,34 @@ class Logger(object):
 class SanghaBotClient(discord.Client):
     def __init__(self, logger):
         self.logger = logger
+        self.prefix = "!stp"
         super().__init__()
+
+    def is_from_self(self, message):
+        return message.author == self.user
+
+    def is_message_for_us(self, message):
+        return message.content.startswith(self.prefix)
+
+    def normalise_message(self, message):
+        self.logger.log(LogLevel.DEBUG, f"Normalising message: {message}")
+        message.content = message.content.lower().lstrip(self.prefix).strip()
+        return message
+
+    async def process_message(self, normalised_message):
+        self.logger.log(LogLevel.INFO, f"Processing message content: {normalised_message.content}")
+        await normalised_message.channel.send(normalised_message.content)
 
     async def on_ready(self):
         self.logger.log(LogLevel.INFO, "Bot ready to go!")
+
+    async def on_message(self, message):
+        self.logger.log(LogLevel.DEBUG, f"Got message: {message}")
+        if self.is_message_for_us(message):
+            if self.is_from_self(message):
+                await message.channel.send("Nice try :wink:")
+            else:
+                await self.process_message(self.normalise_message(message))
 
 def start_bot(logger, discord_token):
     logger.log(LogLevel.DEBUG, f"Starting bot: __name__ == {__name__}")
